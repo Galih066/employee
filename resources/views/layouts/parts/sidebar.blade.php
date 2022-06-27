a4d8b19260287876f8b5b247198800c4b2dc6f8d
Galih066 <galihdapaimanda@gmail.com>
2022-06-27 23:30:56 +0800

    Add authorization for admin and employee

---
+++ resources/views/layouts/parts/sidebar.blade.php
@@ -0,0 +1,15 @@
+<div class="border-end bg-white" id="sidebar-wrapper">
+    @can('admin')
+        <div class="sidebar-heading border-bottom bg-light">Admin Employee</div>
+        <div class="list-group list-group-flush">
+            <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Dashboard</a>
+            <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Shortcuts</a>
+            <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Overview</a>
+            <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Events</a>
+            <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Profile</a>
+            <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Status</a>
+        </div>
+    @else
+        <div class="sidebar-heading border-bottom bg-light">Employee</div>
+    @endcan
+</div>

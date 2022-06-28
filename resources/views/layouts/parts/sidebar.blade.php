0233a4385e784e2bbf9e87a133703badfcf1fef2
Galih066 <galihdapaimanda@gmail.com>
2022-06-28 22:17:29 +0800

    Fix blade template layout

--- resources/views/layouts/parts/sidebar.blade.php
+++ resources/views/layouts/parts/sidebar.blade.php
@@ -1,6 +1,6 @@
-<div class="border-end bg-white" id="sidebar-wrapper">
+<div class="bg-white" id="sidebar-wrapper">
     @can('admin')
-        <div class="sidebar-heading border-bottom bg-light">Admin Employee</div>
+        <div class="sidebar-heading bg-white">Admin Employee</div>
         <div class="list-group list-group-flush">
             <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Dashboard</a>
             <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Shortcuts</a>
@@ -10,7 +10,7 @@
             <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Status</a>
         </div>
     @else
-        <div class="sidebar-heading border-bottom bg-light">Employee</div>
+        <div class="sidebar-heading bg-white">Employee</div>
         <div class="list-group list-group-flush">
             <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Profile</a>
         </div>

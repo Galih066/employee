ffda0ea3e2aa6dc09c16968b31c8a56ff31d7398
Galih066 <galihdapaimanda@gmail.com>
2022-06-30 08:01:58 +0800

    Create resume migration

--- resources/views/employee/profile/index.blade.php
+++ resources/views/employee/profile/index.blade.php
@@ -7,16 +7,16 @@
 @section('dashboard-content')
     <div>
         <div class="row">
-            <div class="col-md-4">
+            <div class="col-md-3">
                 <div class="card card-body">
-                    <img class="rounded-circle mx-auto border border-2" src="{{ Avatar::create(Auth::user()->name)->toBase64() }}" alt="user-img">
+                    <img class="rounded-circle mx-auto" src="{{ Avatar::create(Auth::user()->name)->toBase64() }}" alt="user-img">
                     <div class="text-center my-3">
                         <p class="m-0 fw-bold">{{ Auth::user()->name }}</p>
                         <p class="small fst-italic text-secondary">{{ Auth::user()->email }}</p>
                     </div>
                 </div>
             </div>
-            <div class="col-md-8">test</div>
+            <div class="col-md-9">test</div>
         </div>
     </div>
 @endsection

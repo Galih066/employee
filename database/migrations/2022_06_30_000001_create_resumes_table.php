8bd8db2b8e95daa1945dafe33664adf2876cd0cb
Galih066 <galihdapaimanda@gmail.com>
2022-07-11 21:58:42 +0800

    Add table resume migration

--- database/migrations/2022_06_30_000001_create_resumes_table.php
+++ database/migrations/2022_06_30_000001_create_resumes_table.php
@@ -15,6 +15,44 @@ class CreateResumesTable extends Migration
     {
         Schema::create('resumes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('place_of_birth')->nullable();
+            $table->date('date_of_birth')->nullable();
+            $table->enum('gender', ['Male', 'Female'])->nullable();
+            $table->foreignId('religion_id')->nullable();
+            $table->string('ethnic', 150)->nullable();
+            $table->string('address')->nullable();
+            $table->char('rt')->nullable();
+            $table->char('rw')->nullable();
+            $table->string('kelurahan')->nullable();
+            $table->string('kecamatan')->nullable();
+            $table->string('city')->nullable();
+            $table->string('kodepos')->nullable();
+            $table->enum('married_status', ['Married', 'Not Married'])->nullable();
+            $table->string('father_name')->nullable();
+            $table->date('father_dob')->nullable();
+            $table->string('father_address')->nullable();
+            $table->string('father_education')->nullable();
+            $table->string('father_job')->nullable();
+            $table->string('father_status')->nullable();
+            $table->string('mother_name')->nullable();
+            $table->date('mother_dob')->nullable();
+            $table->string('mother_address')->nullable();
+            $table->string('mother_education')->nullable();
+            $table->string('mother_job')->nullable();
+            $table->string('mother_status')->nullable();
+            $table->string('junior_school_name')->nullable();
+            $table->string('junior_school_city')->nullable();
+            $table->string('junior_school_year_enter')->nullable();
+            $table->string('junior_school_year_graduate')->nullable();
+            $table->string('junior_hi_school_name')->nullable();
+            $table->string('junior_hi_school_city')->nullable();
+            $table->string('junior_hi_school_year_enter')->nullable();
+            $table->string('junior_hi_school_year_graduate')->nullable();
+            $table->string('senior_hi_school_name')->nullable();
+            $table->string('senior_hi_school_city')->nullable();
+            $table->string('senior_hi_school_year_enter')->nullable();
+            $table->string('senior_hi_school_year_graduate')->nullable();
             $table->timestamps();
         });
     }

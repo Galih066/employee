4bf43039611a7af71ba9ac8534bb2c3b83d7530c
Galih066 <galihdapaimanda@gmail.com>
2022-08-07 21:25:34 +0800

    Add update parent data form and font awesome

--- resources/views/layouts/parts/profile/parentdata.blade.php
+++ resources/views/layouts/parts/profile/parentdata.blade.php
@@ -1,8 +1,11 @@
 <form method="POST" action="{{ route('updatePersonalData') }}">
     @csrf
-    <p class="fw-semibold">Father</p>
+    <p class="fw-semibold mt-3">
+        <i class="fa-solid fa-person me-2"></i>
+        Father's data
+    </p>
     <div class="row mb-3">
-        <label class="col-md-3 col-form-label text-capitalize text-muted" for="kecamatan">Name</label>
+        <label class="col-md-3 col-form-label text-capitalize text-muted">Name</label>
         <div class="col-md-9">
             <input name="fatherName" value="{{ old('fatherName') }}" type="text" class="form-control" placeholder="Father's name">
             @error('fatherName')
@@ -11,7 +14,7 @@
         </div>
     </div>
     <div class="row mb-3">
-        <label class="col-md-3 col-form-label text-capitalize text-muted" for="kecamatan">Address</label>
+        <label class="col-md-3 col-form-label text-capitalize text-muted">Address</label>
         <div class="col-md-9">
             <input name="fatherAddress" value="{{ old('fatherAddress') }}" type="text" class="form-control" placeholder="Father's address">
             @error('fatherAddress')
@@ -60,6 +63,75 @@
                 <option value="2">Passed Away</option>
             </select>
             @error('fatherStatus')
+                <p class="small text-danger">{{ $message }}</p>
+            @enderror
+        </div>
+    </div>
+
+    <p class="fw-semibold mt-5">
+        <i class="fa-solid fa-person-dress me-2"></i>
+        Mother's data
+    </p>
+    
+    <div class="row mb-3">
+        <label class="col-md-3 col-form-label text-capitalize text-muted">Name</label>
+        <div class="col-md-9">
+            <input name="motherName" value="{{ old('motherName') }}" type="text" class="form-control" placeholder="mother's name">
+            @error('motherName')
+                <p class="small text-danger">{{ $message }}</p>
+            @enderror
+        </div>
+    </div>
+    <div class="row mb-3">
+        <label class="col-md-3 col-form-label text-capitalize text-muted">Address</label>
+        <div class="col-md-9">
+            <input name="motherAddress" value="{{ old('motherAddress') }}" type="text" class="form-control" placeholder="mother's address">
+            @error('motherAddress')
+                <p class="small text-danger">{{ $message }}</p>
+            @enderror
+        </div>
+    </div>
+    <div class="row mb-3">
+        <label class="col-md-3 col-form-label text-capitalize text-muted" for="education">education</label>
+        <div class="col-md-9">
+            <select class="form-select" name="motherEducation" id="education" required>    
+                <option value="">Choose education</option>
+                @forelse ($jenjang as $item)
+                    <option value="{{ $item->id ?? '' }}">{{ $item->name ?? '' }}</option>
+                @empty
+                    <option>No education data</option>
+                @endforelse
+            </select>
+            @error('motherEducation')
+                <p class="small text-danger">{{ $message }}</p>
+            @enderror
+        </div>
+    </div>
+    <div class="row mb-3">
+        <label class="col-md-3 col-form-label text-capitalize text-muted" for="motherJob">Jobs</label>
+        <div class="col-md-9">
+            <select class="form-select" name="motherJob" id="motherJob" required>    
+                <option value="">Choose mother's jobs</option>
+                @forelse ($job as $item)
+                    <option value="{{ $item->id ?? '' }}">{{ $item->name ?? '' }}</option>
+                @empty
+                    <option>No job's data</option>
+                @endforelse
+            </select>
+            @error('motherJob')
+                <p class="small text-danger">{{ $message }}</p>
+            @enderror
+        </div>
+    </div>
+    <div class="row mb-3">
+        <label class="col-md-3 col-form-label text-capitalize text-muted" for="motherStatus">Status</label>
+        <div class="col-md-9">
+            <select class="form-select" name="motherStatus" id="motherStatus" required>    
+                <option value="">Choose mother's status</option>
+                <option value="1">Still Alive</option>
+                <option value="2">Passed Away</option>
+            </select>
+            @error('motherStatus')
                 <p class="small text-danger">{{ $message }}</p>
             @enderror
         </div>
